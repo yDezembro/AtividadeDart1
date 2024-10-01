050fde3864a43d2fb965516037139034d217d49b
yDezembro <164434984+yDezembro@users.noreply.github.com>
2024-10-01 20:19:09 -0300

    Create widget_test.dart

---
+++ widget_test.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_app/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  
+    await tester.pumpWidget(const MyApp());
+    await tester.pumpAndSettle();
+  
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pumpAndSettle();
+
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
